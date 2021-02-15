e40f8a3d316e371a5a55a385dc373ee26ef10371
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-02-15 18:23:52 +0000

    Added hpm counter test and enabled performance counters in build.

--- Tests/small-tests/hpm.c
+++ Tests/small-tests/hpm.c
@@ -1,3 +1,10 @@
 int main(void) {
-    return 0;
+    int ret;
+    asm volatile (
+        "li t0, 0x6; csrw mhpmevent3, t0; csrr t1, mhpmcounter3; auipc t0, 0x0; csrr t0, mhpmcounter3; sub %0, t0, t1"
+        : "=r"(ret) //output
+        : //input
+        : "t0", "t1" //clobbered
+    );
+    return ret - 1; //Expecting ret to be 1 because only 1 auipc instruction happened.
 }
