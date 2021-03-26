743373ae41d43816cc68af0bcd6bcf7dd05ce5da
Marno <Marno.van-der-Maas@cl.cam.ac.uk>
2021-03-26 15:44:34 +0000

    Added memory shim test

--- Tests/small-tests/build.sh
+++ Tests/small-tests/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 
 riscv64-unknown-elf-gcc -o init.o -c init.s
-for TEST in toy dual hpm
+for TEST in toy dual hpm memshim
 do
 riscv64-unknown-elf-gcc -o $TEST.o -c $TEST.c
 riscv64-unknown-elf-gcc -nostdlib -mcmodel=medany -Tlink.ld -o $TEST $TEST.o init.o
---
+++ Tests/small-tests/memshim.c
@@ -0,0 +1,72 @@
+#define CONFIG_ADDRESS_INCREMENT (64)
+#define CONFIG_ADDRESS_REVOKE    (0x03000000)
+#define CONFIG_ADDRESS_OWN       (CONFIG_ADDRESS_REVOKE + CONFIG_ADDRESS_INCREMENT)
+#define CONFIG_ADDRESS_READ      (CONFIG_ADDRESS_OWN + CONFIG_ADDRESS_INCREMENT)
+#define CONFIG_ADDRESS_INIT      (CONFIG_ADDRESS_READ + CONFIG_ADDRESS_INCREMENT)
+
+enum ConfigOp{
+  ConfigOpRevoke,
+  ConfigOpOwn,
+  ConfigOpRead,
+  ConfigOpInit
+};
+
+int performPraesidioConfigOps(int argAddress, enum ConfigOp op) {
+  volatile int* configAddress;
+  switch (op) {
+    case ConfigOpRevoke:
+      configAddress = CONFIG_ADDRESS_REVOKE;
+      break;
+    case ConfigOpOwn:
+      configAddress = CONFIG_ADDRESS_OWN;
+      break;
+    case ConfigOpRead:
+      configAddress = CONFIG_ADDRESS_READ;
+      break;
+    case ConfigOpInit:
+      configAddress = CONFIG_ADDRESS_INIT;
+      break;
+    default:
+      return -1;
+  }
+  configAddress[0] = argAddress;
+//  asm volatile (
+//    "sw %0 0(%1);"
+//    : //output
+//    : "r"(argAddress), "r"(configAddress) //input
+//    : //clobbered
+//  );
+  return 0;
+}
+
+int main(void) {
+    int ret;
+    //Initialize Praesidio Shim
+    performPraesidioConfigOps(0x80000000, ConfigOpOwn);
+    performPraesidioConfigOps(0x80007000, ConfigOpOwn);
+    performPraesidioConfigOps(0x80730000, ConfigOpRevoke);
+    performPraesidioConfigOps(0xBF7DF000, ConfigOpOwn);
+
+    //Loop that counts AXI4 requests coming out of CPU 
+    asm volatile (
+        "li t0, 0x84;"//Event ID for AXI4 ar
+        //"li t0, 0x6;"//Event ID for AUIPC
+        "csrw mhpmevent3, t0;"
+        "csrr t1, mhpmcounter3;"
+        "li t0, 20;"
+        "loop: addi t0, t0, -1;"
+        "auipc t2, 0x100;"
+        "srli t2, t2, 2;"
+        "slli t2, t2, 2;"
+        "slli t3, t0, 16;"
+        "add t2, t2, t3;"
+        "lw t2, 0(t2);"
+        "bnez t0, loop;"
+        "csrr t0, mhpmcounter3;"
+        "sub %0, t0, t1"
+        : "=r"(ret) //output
+        : //input
+        : "t0", "t1", "t2", "t3"//clobbered
+    );
+    return ret;
+}
